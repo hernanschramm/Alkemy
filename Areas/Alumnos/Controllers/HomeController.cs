d72c00788312bb86cf0fad7d944de9aca5277abe
Hernan Schramm <schrammhernangabriel@hotmail.com>
2020-10-17 07:20:12 -0300

    Guardar todo

--- Areas/Alumnos/Controllers/HomeController.cs
+++ Areas/Alumnos/Controllers/HomeController.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Alkemy.Models;
 
-namespace Alkemy.Controllers
+using Alkemy.Modelos.ViewModels;
+
+namespace Alkemy.Area.Alumnos.Controllers
 {
+    [Area("Alumnos")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
